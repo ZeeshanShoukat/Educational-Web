865ba7479715f5cdc995da294af655dc3060de09
ZeeshanShoukat <zeeshantanooli@gmail.com>
2017-12-04 16:18:33 +0500

    initial

---
+++ EducationalWebsite/App_Start/BundleConfig.cs
@@ -0,0 +1,34 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace EducationalWebsite
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/LibMainjs").Include(
+                                        "~/Content/js/jquery-2.1.4.min.js",
+                                         "~/Content/js/jquery.flexslider.js",
+                                         "~/Content/js/owl.carousel.js",
+                                         "~/Content/js/simplyCountdown.js",
+                                         "~/Content/js/main.js",
+                                         "~/Content/js/move-top.js",
+                                         "~/Content/js/easing.js",
+                                         "~/Content/js/bootstrap.js"
+
+                                                        ));
+            bundles.Add(new StyleBundle("~/bundles/LibMaincss").Include(
+                                         "~/Content/css/bootstrap.css",
+                                          "~/Content/css/font-awesome.css",
+                                           "~/Content/css/flexslider.css",
+                                            "~/Content/css/owl.carousel.css",
+                                             "~/Content/css/style.css"
+
+                ));
+
+        }
+
+    }
+}
