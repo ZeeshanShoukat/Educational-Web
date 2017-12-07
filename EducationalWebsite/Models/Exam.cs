10940aacc129b359e8e47b200c6eac7826d66720
ZeeshanShoukat <zeeshantanooli@gmail.com>
2017-12-07 13:05:27 +0500

    SubjectAdded

---
+++ EducationalWebsite/Models/Exam.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EducationalWebsite.Models
+{
+    public class Exam
+    {
+        public int ID { get; set; }
+        public String Name { get; set; }
+    }
+
+}
